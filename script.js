fede517ac96292f6129a368ecf0c25b5fa075cce
Imran.Programmer <90908160+Imranprogrammer9@users.noreply.github.com>
2022-01-27 10:36:23 +0500

    Dropdown Filter Menu Using HTML CSS JS

---
+++ script.js
@@ -0,0 +1,7 @@
+function show(anything) {
+    document.querySelector('.textBox').value = anything;
+}
+let dropdown = document.querySelector('.dropdown');
+dropdown.onclick = function(){
+    dropdown.classList.toggle('active');    
+}
